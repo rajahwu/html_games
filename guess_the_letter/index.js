fee211212ce0e01b504dabbc867d92fee63c5eab
rajahwu <vmradford@gmail.com>
2022-12-19 06:50:03 -0500

    add image capture button

--- guess_the_letter/index.js
+++ guess_the_letter/index.js
@@ -1,71 +1,86 @@
 window.onload = canvasApp;
-/**
- * Check browser support for canvas
- *
- * @returns boolean
- */
 function canvasSupport() {
     return !!document.createElement('canvas').getContext;
 }
-/**
- * runs canvas app
- *
- * @returns void
- */
 function canvasApp() {
-    var guesses;
-    var message = "Guess The Letter From a (lower) to z (higher)";
-    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    if (!canvasSupport)
+        throw new Error('HTML5 Canvas is not supported by your browser');
+    var canvasOne = document.getElementById('canvas_one');
+    var ctx = canvasOne.getContext('2d');
+    if (!ctx)
+        throw new Error('Canvas must have rendering Conxtext');
     var today = new Date;
-    var letterToGuess = "";
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var message = "Guess The Letter From a (lower) to z (higher)";
     var lettersGussed = [];
-    var letterIndex;
-    var guessIndex;
-    var gameOver = false;
+    var letterToGuess;
+    var guesses;
     var higherOrLower;
+    var guessIndex;
+    var letterIndex;
+    var gameOver = false;
+    initGame();
     function initGame() {
-        letterIndex = Math.floor(Math.random() * letters.length);
-        letterToGuess = letters[letterIndex];
+        var formElement = document.getElementById('createImageData');
+        formElement === null || formElement === void 0 ? void 0 : formElement.addEventListener('click', createImageDataPressed, false);
+        letterToGuess = letters[Math.floor(Math.random() * letters.length)];
         guesses = 0;
         window.addEventListener('keypress', eventKeyPressed, true);
         drawScreen();
     }
     function eventKeyPressed(e) {
         if (!gameOver) {
-            var letterPressed = e.key
-                .toLocaleLowerCase();
+            var letterPressed_1 = e.key.toLowerCase();
             guesses++;
-            lettersGussed.push(letterPressed);
-            if (letterPressed === letterToGuess) {
-                gameOver = true;
+            lettersGussed.push(letterPressed_1);
+            checkGuessedLetter();
+            function checkGuessedLetter() {
+                if (letterPressed_1 === letterToGuess) {
+                    gameOver = true;
+                    window.removeEventListener('keypress', eventKeyPressed);
+                }
+                else {
+                    letterIndex = letters.indexOf(letterToGuess);
+                    guessIndex = letters.indexOf(letterPressed_1);
+                }
+                if (guessIndex < 0) {
+                    higherOrLower = "That is not a letter";
+                }
+                else if (guessIndex > letterIndex) {
+                    higherOrLower = "Lower";
+                }
+                else {
+                    higherOrLower = "Higher";
+                }
             }
-            else {
-                letterIndex = letters.indexOf(letterToGuess);
-                guessIndex = letters.indexOf(letterPressed);
-            }
-            if (guessIndex < 0) {
-                higherOrLower = "That is not a letter";
-            }
-            else if (guessIndex > letterIndex) {
-                higherOrLower = "Lower";
-            }
-            else {
-                higherOrLower = "Higher";
-            }
-            drawScreen();
         }
+        drawScreen();
     }
-    if (!canvasSupport)
-        return;
-    var canvasOne = document.getElementById('canvas_one');
-    var ctx = canvasOne.getContext('2d');
-    if (!ctx)
-        return;
-    initGame();
-    /**
-     *  Draw on canvas
-     */
+    function createImageDataPressed() {
+        window.open(canvasOne.toDataURL(), 'canvasImage', 'left=0,top=0,width=' + canvasOne.width + ', height=' + canvasOne.height + ', toobar=0,resizable=0');
+    }
     function drawScreen() {
-        return;
+        ctx.fillStyle = "#ffffaa";
+        ctx.fillRect(0, 0, 490, 290);
+        ctx.strokeStyle = "#000000";
+        ctx.strokeRect(5, 5, 490, 290);
+        ctx.textBaseline = "top";
+        ctx.fillStyle = "#000000";
+        ctx.font = "10px Sans-Serif";
+        ctx.fillText(today.toLocaleDateString(), 150, 10);
+        ctx.fillStyle = "ff0000";
+        ctx.font = "14px San-Serif";
+        ctx.fillText(message, 125, 30);
+        ctx.fillStyle = "#109910;";
+        ctx.font = "16px San-Serif";
+        ctx.fillText('Guesses: ' + guesses, 215, 50);
+        ctx.fillStyle = "#000000";
+        ctx.font = "16px Sans-Serif";
+        ctx.fillText("Letters Guessed: " + lettersGussed.toString(), 10, 260);
+        if (gameOver) {
+            ctx.fillStyle = "ff0000";
+            ctx.font = "40px Sans-Serif";
+            ctx.fillText("You Got It!", 150, 180);
+        }
     }
 }
--- guess_the_letter/index.ts
+++ guess_the_letter/index.ts
@@ -1,73 +1,95 @@
 window.onload = canvasApp;
-/**
- * Check browser support for canvas
- * 
- * @returns boolean
- */
+
+
 function canvasSupport(): boolean {
     return !!document.createElement('canvas').getContext;
 }
 
-/**
- * runs canvas app
- * 
- * @returns void
- */
 function canvasApp(): void {
-    let guesses: number;
-    const message = "Guess The Letter From a (lower) to z (higher)";
-    const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    const today = new Date;
-    let letterToGuess = "";
-    const lettersGussed: string[] = [];
-    let letterIndex: number;
-    let guessIndex: number;
-    let gameOver = false;
-    let higherOrLower: "That is not a letter" | "Higher" | "Lower";
-
-    function initGame() {
-        letterIndex = Math.floor(Math.random() * letters.length);
-        letterToGuess = letters[letterIndex];
-        guesses = 0;
-        window.addEventListener('keypress', eventKeyPressed, true);
-        drawScreen()
-    }
-    
-    function eventKeyPressed(e: KeyboardEvent) {
-        if(!gameOver) {
-            let letterPressed = e.key
-            .toLocaleLowerCase()
-            guesses++;
-            lettersGussed.push(letterPressed);
-
-            if (letterPressed === letterToGuess) {
-                gameOver = true;
-            } else {
-                letterIndex = letters.indexOf(letterToGuess)
-                guessIndex = letters.indexOf(letterPressed)
-            }
-            if (guessIndex < 0) {
-                higherOrLower = "That is not a letter";
-            } else if (guessIndex > letterIndex) {
-                higherOrLower = "Lower";
-            } else {
-                higherOrLower = "Higher";
-            }
-            drawScreen();
-        }
-    }
-
-    if (!canvasSupport) return;
+    if (!canvasSupport) throw new Error('HTML5 Canvas is not supported by your browser');
     const canvasOne = <HTMLCanvasElement>document.getElementById('canvas_one');
     const ctx = <CanvasRenderingContext2D>canvasOne.getContext('2d');
-    if (!ctx) return;
+    if (!ctx) throw new Error('Canvas must have rendering Conxtext');
+
+    const today = new Date;
+    const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const message = "Guess The Letter From a (lower) to z (higher)";
+    const lettersGussed: string[] = [];
+    let letterToGuess: string;
+    let guesses: number;
+    let higherOrLower: "That is not a letter" | "Higher" | "Lower";
+    let guessIndex: number;
+    let letterIndex: number;
+    let gameOver = false;
     initGame();
 
+    function initGame(): void {
+        const formElement = document.getElementById('createImageData');
+        formElement?.addEventListener('click', createImageDataPressed, false);
+        letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+        guesses = 0;
+        window.addEventListener('keypress', eventKeyPressed, true);
+        drawScreen();
+    }
 
-    /**
-     *  Draw on canvas
-     */
+    function eventKeyPressed(e: KeyboardEvent): void {
+        if (!gameOver) {
+            const letterPressed = e.key.toLowerCase();
+            guesses++;
+            lettersGussed.push(letterPressed);
+            checkGuessedLetter();
+
+            function checkGuessedLetter(): void {
+                if (letterPressed === letterToGuess) {
+                    gameOver = true;
+                    window.removeEventListener('keypress', eventKeyPressed);
+                } else {
+                    letterIndex = letters.indexOf(letterToGuess);
+                    guessIndex = letters.indexOf(letterPressed);
+                }
+                if (guessIndex < 0) {
+                    higherOrLower = "That is not a letter";
+                } else if (guessIndex > letterIndex) {
+                    higherOrLower = "Lower";
+                } else {
+                    higherOrLower = "Higher";
+                }
+            }
+        }
+        drawScreen();
+    }
+
+    function createImageDataPressed() {
+        window.open(canvasOne.toDataURL(), 'canvasImage', 'left=0,top=0,width=' + canvasOne.width + ', height=' + canvasOne.height + ', toobar=0,resizable=0')
+    }
+
     function drawScreen(): void {
-        return
+        ctx.fillStyle = "#ffffaa";
+        ctx.fillRect(0, 0, 490, 290);
+
+        ctx.strokeStyle = "#000000";
+        ctx.strokeRect(5, 5, 490, 290);
+        ctx.textBaseline = "top";
+
+        ctx.fillStyle = "#000000";
+        ctx.font = "10px Sans-Serif";
+        ctx.fillText(today.toLocaleDateString(), 150, 10);
+
+        ctx.fillStyle = "ff0000";
+        ctx.font = "14px San-Serif";
+        ctx.fillText(message, 125, 30);
+        ctx.fillStyle = "#109910;"
+        ctx.font = "16px San-Serif";
+        ctx.fillText('Guesses: ' + guesses, 215, 50);
+
+        ctx.fillStyle = "#000000";
+        ctx.font = "16px Sans-Serif";
+        ctx.fillText("Letters Guessed: " + lettersGussed.toString(), 10, 260);
+
+        if (gameOver) {
+            ctx.fillStyle = "ff0000";
+            ctx.font = "40px Sans-Serif";
+            ctx.fillText("You Got It!", 150, 180)
+        }
     }
 }
